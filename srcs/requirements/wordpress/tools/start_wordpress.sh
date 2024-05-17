d705aa7e236cca01a0d228954692f5b67223b9aa
Anais Augu <aaugu@c2r9s5.42lausanne.ch>
2024-05-17 14:51:54 +0200

    wordpress config + api key env variables added

--- srcs/requirements/wordpress/tools/start_wordpress.sh
+++ srcs/requirements/wordpress/tools/start_wordpress.sh
@@ -3,10 +3,35 @@
 #                                                         :::      ::::::::    #
 #    start_wordpress.sh                                 :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: aaugu <aaugu@student.42.fr>                +#+  +:+       +#+         #
+#    By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/30 11:02:35 by aaugu             #+#    #+#              #
-#    Updated: 2024/04/30 11:13:12 by aaugu            ###   ########.fr        #
+#    Updated: 2024/05/17 14:15:09 by aaugu            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+echo "------------------------------- WORDPRESS START -----------------------------------"
+
+php-fpm8.0 -v
+
+# Wait for mariadb to start
+while ! mariadb -u ${MDB_USER} --password=${MDB_PASS} -h mariadb -P 3306 --silent; do
+	sleep 1
+	echo "Mariadb is not ready yet"
+done
+
+# Display database
+echo "-------------------------------\n"
+mariadb -u ${MDB_USER} --password=${MDB_PASS} -h mariadb -P 3306 -e "SHOW DATABASES;"
+echo "-------------------------------\n"
+
+# WP Config
+	# Connect to database
+wp config create --dbname=${DB_NAME} --dbuser=${MDB_USER} --dbpass=${MDB_PASS} --dbhost=${WP_HOST} --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	# Admin config
+wp core install --url=${NGINX_HOST} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_MAIL} --skip-email --allow-root
+	# User creation
+wp user create ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASS} --allow-root
+
+# Start php-fpm in the foreground
+php-fpm8.0 -F
